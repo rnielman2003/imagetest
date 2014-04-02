406d2265c3223c92694e4f353556e7fd4b8bf8db
Ryan <nielson.ryan@gmail.com>
2014-04-01 21:56:27 -0600

    2nd box put in there still not styled

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.button-linkone').click(function() {
+        var bla = $('.centered #boxone').val();
+        console.log(bla);
+    });
+    $('.button-linktwo').click(function() {
+        var bla = $('.centered #boxtwo').val();
+        console.log(bla);
+    });
+});
